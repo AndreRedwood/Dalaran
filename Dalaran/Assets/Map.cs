9f5b937d418398d88df5e1874dff1281e5638b7e
AndreSirko <jasinski9948@gmail.com>
2024-09-30 17:27:26 +0200

    reworking tiles and adding height variance

--- Dalaran/Assets/Map.cs
+++ Dalaran/Assets/Map.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
     [SerializeField] private int mapWidth, mapHeight;
+	[SerializeField] private int[,] map;
 	[SerializeField] private GameObject tilePrefab;
 
 	private void Awake()
 	{
-		GenerateMap();
+		map = new int[5, 5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1 }, { 0, 1, 1, 2, 1 }, { 0, 0, 1, 2, 1 }, { 0, 1, 1, 1, 1 } };
+		GenerateMap(map);
 	}
 
-	private void GenerateMap()
+	private void GenerateMap(int[,] map)
 	{
 		for(int x = 0; x < mapWidth; x++)
 		{
@@ -21,6 +23,7 @@ public class Map : MonoBehaviour
 				float xOffset = x * 0.5f + y * -0.5f;
 				float yOffset = x * 0.25f + y * 0.25f;
 				GameObject tile = Instantiate(tilePrefab, new Vector3(xOffset, yOffset, 0), Quaternion.identity);
+				tile.GetComponent<Tile>().SetHeight(map[x,y]);
 			}
 		}
 	}
--- Dalaran/Assets/Tile.cs
+++ Dalaran/Assets/Tile.cs
@@ -8,10 +8,24 @@ public class Tile : MonoBehaviour
 	private PolygonCollider2D collider;
 	private SpriteRenderer spriteRenderer;
 
+	[SerializeField] private Sprite h0, h1, h2;
+	private int height = 0;
+
 	private void Awake()
 	{
 		collider = GetComponent<PolygonCollider2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void SetHeight(int height)
+	{
+		this.height = height;
+		switch (height)
+		{
+			case 0: spriteRenderer.sprite = h0; collider.offset = new Vector2(0, 0); break;
+			case 1: spriteRenderer.sprite = h1; collider.offset = new Vector2(0, 0.25f); break;
+			case 2: spriteRenderer.sprite = h2; collider.offset = new Vector2(0, 0.5f); break;
+		}
 	}
 
 	private void OnMouseEnter()
